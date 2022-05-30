e9c4b9fdb3a3ca331f911a52cd4b7958564b1560
rachell <dazzlemevanity@gmail.com>
2022-05-29 20:27:19 -0400

    commit tree cc9a447fa16633d194ed376a23b24c75d73d0881

---
+++ index.js
@@ -0,0 +1,11 @@
+// function divide () {
+// let a = 2000
+// let b = 100
+// return (a/b)
+// }
+
+const divide = (a,b) => a/b
+
+const square = (number1) => number1 * number1
+
+const add = (thing1, thing2) => thing1 + thing2
